a314bc163d1110f06aba7f6026c879705f3fe7f5
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-01-24 14:03:21 -0430

    Added Course model

--- models/models.py
+++ models/models.py
@@ -13,3 +13,9 @@ from openerp import models, fields, api
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
