ca3156a092885541555f06a7bf45cc9570b49ffe
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-01-27 15:41:54 -0430

    Added default values for "start_date" and "active" fields for Session model

--- models/models.py
+++ models/models.py
@@ -29,9 +29,11 @@ class Session(models.Model):
     _name = 'openacademy.session'
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
+    start_date = fields.Date(default=fields.Date.today)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+    active = fields.Boolean(default=True)
+
     instructor_id = fields.Many2one('res.partner', string="Instructor",
         domain=['|', ('instructor', '=', True),
                      ('category_id.name', 'ilike', "Teacher")])
