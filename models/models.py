b901b93f1ee3a423871ed6b65644e6a1785a4bb9
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-01-26 20:28:21 -0430

    Added Inverse one2many relations between Course and Session models

--- models/models.py
+++ models/models.py
@@ -21,6 +21,8 @@ class Course(models.Model):
     description = fields.Text()
     responsible_id = fields.Many2one('res.users',
         ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many(
+        'openacademy.session', 'course_id', string="Sessions")
 
 
 class Session(models.Model):
