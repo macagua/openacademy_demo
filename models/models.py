bc5828ad7c596c4de7b3f91c55fa7c12673e65b3
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-01-26 20:31:56 -0430

    Added Multiple many2many relations between Session and res.partner models

--- models/models.py
+++ models/models.py
@@ -35,3 +35,5 @@ class Session(models.Model):
     instructor_id = fields.Many2one('res.partner', string="Instructor")
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
