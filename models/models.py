0a6167a0149586a154826c623c1c68f08fee1e85
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-01-27 13:37:18 -0430

    Added More complex domains for Session model and Partner categories

--- models/models.py
+++ models/models.py
@@ -33,7 +33,9 @@ class Session(models.Model):
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one('res.partner', string="Instructor",
-        domain=[('instructor', '=', True)])
+        domain=['|', ('instructor', '=', True),
+                     ('category_id.name', 'ilike', "Teacher")])
+
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
