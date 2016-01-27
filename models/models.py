e70f6cf70e01fce4ea05621510ed2a5c1efc6a88
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-01-27 15:30:21 -0430

    Added a simple Computed field for "taken seats" for Session model

--- models/models.py
+++ models/models.py
@@ -35,8 +35,16 @@ class Session(models.Model):
     instructor_id = fields.Many2one('res.partner', string="Instructor",
         domain=['|', ('instructor', '=', True),
                      ('category_id.name', 'ilike', "Teacher")])
-
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        for r in self:
+            if not r.seats:
+                r.taken_seats = 0.0
+            else:
+                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
 
