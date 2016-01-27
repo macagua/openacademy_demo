6565032b5c9a96d63dc7b473d9d197e462787595
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-01-27 13:02:25 -0430

    Added a simple Domain on relational fields for Session Model

--- models/models.py
+++ models/models.py
@@ -32,7 +32,8 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
-    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    instructor_id = fields.Many2one('res.partner', string="Instructor",
+        domain=[('instructor', '=', True)])
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
