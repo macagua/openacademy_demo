ec12f0e5bd27f264fe9d3147ba64c2056dc7d00e
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-01-26 20:20:46 -0430

    Added Many2one relations between Course and Session models

--- models/models.py
+++ models/models.py
@@ -19,11 +19,17 @@ class Course(models.Model):
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+    responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsible", index=True)
+
 
 class Session(models.Model):
-	_name = 'openacademy.session'
+    _name = 'openacademy.session'
 
-	name = fields.Char(required=True)
-	start_date = fields.Date()
-	duration = fields.Float(digits=(6, 2), help="Duration in days")
-	seats = fields.Integer(string="Number of seats")
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course',
+        ondelete='cascade', string="Course", required=True)
