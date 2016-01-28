01d944098cbff935e5a97db390bc37340b8a59f0
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-01-27 21:31:53 -0430

    Added a duplicate option of Action form called "Duplicate" for Course model

--- models/models.py
+++ models/models.py
@@ -24,6 +24,20 @@ class Course(models.Model):
     session_ids = fields.One2many(
         'openacademy.session', 'course_id', string="Sessions")
 
+    @api.multi
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Course, self).copy(default)
+
     _sql_constraints = [
         ('name_description_check',
          'CHECK(name != description)',
